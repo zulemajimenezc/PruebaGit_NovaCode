c1a75a2f5302bef2614d708e1963e927d9a6ca61
Zulema Jimenez <zcjime02@gmail.com>
2019-05-06 19:51:13 -0500

    first commit

---
+++ app/src/main/java/com/example/pruebagit/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.pruebagit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button pulsa = (Button) findViewById(R.id.pulsa);
+
+        pulsa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Cambio de Zulema",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
